fac4164d447e4ed42dbd657749b931bed01e0d17
Shapovalenko Egor <73957529+lastikp0@users.noreply.github.com>
2024-09-26 23:03:42 +0300

    Update ship.cpp

--- BattleShip/source/ship.cpp
+++ BattleShip/source/ship.cpp
@@ -77,6 +77,8 @@ Ship::ShipSegment::ShipSegment()
     health_ = kMaxHealth;
 }
 
+Ship::ShipSegment::ShipSegment() = default;
+
 void Ship::ShipSegment::takeDamege(int damage)
 {
     if (damage < 0) {
